0627d1ea500a1fc36f422611c042e8f4d25601e9
lisabeillle <lisa.peillon98@gmail.com>
2022-06-14 15:19:09 +0200

    +- Dao tests

--- app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/MainActivityInstrumentedTest.java
@@ -10,6 +10,7 @@ import static com.cleanup.todoc.TestUtils.withRecyclerView;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.cleanup.todoc.ui.MainActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,93 +33,159 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
-    @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+          @Rule
+          public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void addAndRemoveTask() {
-        MainActivity activity = rule.getActivity();
-        TextView lblNoTask = activity.findViewById(R.id.lbl_no_task);
-        RecyclerView listTasks = activity.findViewById(R.id.list_tasks);
+          @Test
+          public void addAndRemoveTask() {
+                    MainActivity activity = rule.getActivity();
+                    TextView lblNoTask = activity.findViewById(R.id.lbl_no_task);
+                    RecyclerView listTasks = activity.findViewById(R.id.list_tasks);
 
-        onView(withId(R.id.fab_add_task)).perform(click());
-        onView(withId(R.id.txt_task_name)).perform(replaceText("Tâche example"));
-        onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
 
-        // Check that lblTask is not displayed anymore
-        assertThat(lblNoTask.getVisibility(), equalTo(View.GONE));
-        // Check that recyclerView is displayed
-        assertThat(listTasks.getVisibility(), equalTo(View.VISIBLE));
-        // Check that it contains one element only
-        assertThat(listTasks.getAdapter().getItemCount(), equalTo(1));
+                    // Check that lblTask is not displayed anymore
+                    assertThat(lblNoTask.getVisibility(), equalTo(View.GONE));
+                    // Check that recyclerView is displayed
+                    assertThat(listTasks.getVisibility(), equalTo(View.VISIBLE));
+                    // Check that it contains one element only
+                    assertThat(listTasks.getAdapter().getItemCount(), equalTo(1));
 
-        onView(withId(R.id.img_delete)).perform(click());
+                    onView(withId(R.id.img_delete)).perform(click());
 
-        // Check that lblTask is displayed
-        assertThat(lblNoTask.getVisibility(), equalTo(View.VISIBLE));
-        // Check that recyclerView is not displayed anymore
-        assertThat(listTasks.getVisibility(), equalTo(View.GONE));
-    }
+                    // Check that lblTask is displayed
+                    assertThat(lblNoTask.getVisibility(), equalTo(View.VISIBLE));
+                    // Check that recyclerView is not displayed anymore
+                    assertThat(listTasks.getVisibility(), equalTo(View.GONE));
+          }
 
-    @Test
-    public void sortTasks() {
-        MainActivity activity = rule.getActivity();
+          @Test
+          public void sortTasksAlphabetical() {
+                    MainActivity activity = rule.getActivity();
 
-        onView(withId(R.id.fab_add_task)).perform(click());
-        onView(withId(R.id.txt_task_name)).perform(replaceText("aaa Tâche example"));
-        onView(withId(android.R.id.button1)).perform(click());
-        onView(withId(R.id.fab_add_task)).perform(click());
-        onView(withId(R.id.txt_task_name)).perform(replaceText("zzz Tâche example"));
-        onView(withId(android.R.id.button1)).perform(click());
-        onView(withId(R.id.fab_add_task)).perform(click());
-        onView(withId(R.id.txt_task_name)).perform(replaceText("hhh Tâche example"));
-        onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("aaa Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("zzz Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("hhh Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
 
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
-                .check(matches(withText("aaa Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
-                .check(matches(withText("zzz Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
-                .check(matches(withText("hhh Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
 
-        // Sort alphabetical
-        onView(withId(R.id.action_filter)).perform(click());
-        onView(withText(R.string.sort_alphabetical)).perform(click());
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
-                .check(matches(withText("aaa Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
-                .check(matches(withText("hhh Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
-                .check(matches(withText("zzz Tâche example")));
+                    // Sort alphabetical
+                    onView(withId(R.id.action_filter)).perform(click());
+                    onView(withText(R.string.sort_alphabetical)).perform(click());
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+          }
 
-        // Sort alphabetical inverted
-        onView(withId(R.id.action_filter)).perform(click());
-        onView(withText(R.string.sort_alphabetical_invert)).perform(click());
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
-                .check(matches(withText("zzz Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
-                .check(matches(withText("hhh Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
-                .check(matches(withText("aaa Tâche example")));
+          @Test
+          public void sortTasksAlphabeticalInverted() {
+                    MainActivity activity = rule.getActivity();
 
-        // Sort old first
-        onView(withId(R.id.action_filter)).perform(click());
-        onView(withText(R.string.sort_oldest_first)).perform(click());
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
-                .check(matches(withText("aaa Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
-                .check(matches(withText("zzz Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
-                .check(matches(withText("hhh Tâche example")));
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("aaa Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("zzz Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("hhh Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
 
-        // Sort recent first
-        onView(withId(R.id.action_filter)).perform(click());
-        onView(withText(R.string.sort_recent_first)).perform(click());
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
-                .check(matches(withText("hhh Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
-                .check(matches(withText("zzz Tâche example")));
-        onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
-                .check(matches(withText("aaa Tâche example")));
-    }
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+
+                    // Sort alphabetical inverted
+                    onView(withId(R.id.action_filter)).perform(click());
+                    onView(withText(R.string.sort_alphabetical_invert)).perform(click());
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+          }
+
+          @Test
+          public void sortTasksChronological() {
+                    MainActivity activity = rule.getActivity();
+
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("aaa Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("zzz Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("hhh Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+
+                    // Sort recent first
+                    onView(withId(R.id.action_filter)).perform(click());
+                    onView(withText(R.string.sort_recent_first)).perform(click());
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+          }
+
+          @Test
+          public void sortTasksChronologicalInverted() {
+                    MainActivity activity = rule.getActivity();
+
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("aaa Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("zzz Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+                    onView(withId(R.id.fab_add_task)).perform(click());
+                    onView(withId(R.id.txt_task_name)).perform(replaceText("hhh Tâche example"));
+                    onView(withId(android.R.id.button1)).perform(click());
+
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+
+                    // Sort old first
+                    onView(withId(R.id.action_filter)).perform(click());
+                    onView(withText(R.string.sort_oldest_first)).perform(click());
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(0, R.id.lbl_task_name))
+                              .check(matches(withText("aaa Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(1, R.id.lbl_task_name))
+                              .check(matches(withText("zzz Tâche example")));
+                    onView(withRecyclerView(R.id.list_tasks).atPositionOnView(2, R.id.lbl_task_name))
+                              .check(matches(withText("hhh Tâche example")));
+          }
 }
--- app/src/androidTest/java/com/cleanup/todoc/data/TaskDaoTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/data/TaskDaoTest.java
@@ -10,6 +10,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.cleanup.todoc.model.Task;
 import com.cleanup.todoc.model.TaskAndProject;
+import com.cleanup.todoc.ui.MainActivity;
 import com.cleanup.todoc.utils.LiveDataTestUtils;
 
 import junit.framework.TestCase;
@@ -36,8 +37,7 @@ public class TaskDaoTest extends TestCase {
           List<TaskAndProject> taskAndProjectList;
 
           @Before
-          public void setUp() throws Exception {
-                    super.setUp();
+          public void setUp() {
                     Context context = ApplicationProvider.getApplicationContext();
                     appDatabase = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
                     taskDao = appDatabase.getTaskDao();
@@ -53,8 +53,8 @@ public class TaskDaoTest extends TestCase {
           public void insertAndGetTasks() throws InterruptedException {
                     Task task1 = new Task(2L, "Aspirateur", new Date().getTime());
                     Task task2 = new Task(1L, "Nettoyage des vitres", new Date().getTime());
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
                     taskAndProjectLiveDataList = taskDao.getAllTasksAndProjects();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertEquals(2, taskAndProjectList.size());
@@ -65,9 +65,9 @@ public class TaskDaoTest extends TestCase {
                     Task task1 = new Task(2L, "ggg", new Date().getTime());
                     Task task2 = new Task(3L, "aaa", new Date().getTime());
                     Task task3 = new Task(1L, "zzz", new Date().getTime());
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
-                    taskDao.insertTask(task3);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
+                    executor.execute(() -> taskDao.insertTask(task3));
                     taskAndProjectLiveDataList = taskDao.getTasksFromAToZ();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertSame(task2, taskAndProjectList.get(0));
@@ -80,9 +80,9 @@ public class TaskDaoTest extends TestCase {
                     Task task1 = new Task(2L, "ggg", new Date().getTime());
                     Task task2 = new Task(3L, "aaa", new Date().getTime());
                     Task task3 = new Task(1L, "zzz", new Date().getTime());
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
-                    taskDao.insertTask(task3);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
+                    executor.execute(() -> taskDao.insertTask(task3));
                     taskAndProjectLiveDataList = taskDao.getTasksFromZToA();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertSame(task3, taskAndProjectList.get(0));
@@ -95,9 +95,9 @@ public class TaskDaoTest extends TestCase {
                     Task task1 = new Task(2L, "ggg", 129);
                     Task task2 = new Task(3L, "aaa", 123);
                     Task task3 = new Task(1L, "zzz", 125);
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
-                    taskDao.insertTask(task3);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
+                    executor.execute(() -> taskDao.insertTask(task3));
                     taskAndProjectLiveDataList = taskDao.getTasksFromRecentToOld();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertSame(task2, taskAndProjectList.get(0));
@@ -110,9 +110,9 @@ public class TaskDaoTest extends TestCase {
                     Task task1 = new Task(2L, "ggg", 129);
                     Task task2 = new Task(3L, "aaa", 123);
                     Task task3 = new Task(1L, "zzz", 125);
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
-                    taskDao.insertTask(task3);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
+                    executor.execute(() -> taskDao.insertTask(task3));
                     taskAndProjectLiveDataList = taskDao.getTasksFromOldToRecent();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertSame(task1, taskAndProjectList.get(0));
@@ -125,9 +125,9 @@ public class TaskDaoTest extends TestCase {
                     Task task1 = new Task(2L, "ggg", 129);
                     Task task2 = new Task(3L, "aaa", 123);
                     Task task3 = new Task(1L, "zzz", 125);
-                    taskDao.insertTask(task1);
-                    taskDao.insertTask(task2);
-                    taskDao.insertTask(task3);
+                    executor.execute(() -> taskDao.insertTask(task1));
+                    executor.execute(() -> taskDao.insertTask(task2));
+                    executor.execute(() -> taskDao.insertTask(task3));
                     taskAndProjectLiveDataList = taskDao.getAllTasksAndProjects();
                     taskAndProjectList = LiveDataTestUtils.getValue(taskAndProjectLiveDataList);
                     assertSame(task1, taskAndProjectList.get(0));
